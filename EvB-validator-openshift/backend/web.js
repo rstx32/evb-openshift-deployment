f66d799a3ed5889c4285965933d1a956299b5e59
Restu Indera <rstx32@gmail.com>
2022-08-02 14:38:32 +0700

    debug show account

--- EvB-validator-openshift/backend/web.js
+++ EvB-validator-openshift/backend/web.js
@@ -250,6 +250,7 @@ app.listen(process.env.HTTP_PORT, () => {
 ;(() => {
   const list = process.env.ACCOUNT.split(' ')
   for (let index = 0; index < list.length; index += 2) {
+    console.log(list[index])
     createAccount(list[index], list[index + 1])
   }
 })()
