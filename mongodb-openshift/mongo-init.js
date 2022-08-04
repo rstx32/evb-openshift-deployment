82a7ce3e33038a3109ac9d5a84b467dc24b16c43
Restu Indera <rstx32@gmail.com>
2022-08-04 16:16:52 +0700

    ignore init.js

--- mongodb-openshift/mongo-init.js
+++ mongodb-openshift/mongo-init.js
@@ -1,8 +1,8 @@
 // create dataList database for admin
 db = db.getSiblingDB('dataList')
 db.createUser({
-  user: 'rstx',
-  pwd: 'xtsr',
+  user: 'myuser',
+  pwd: 'mypass',
   roles: [
     {
       role: 'readWrite',
